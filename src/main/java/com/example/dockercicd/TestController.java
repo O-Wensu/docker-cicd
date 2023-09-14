57022d8d455286de334b3175dc340370f41f4a68
sojk401 <sojk401@gmail.com>
2023-09-15 01:47:52 +0900

    Chore: CI/CD 테스트

--- src/main/java/com/example/dockercicd/TestController.java
+++ src/main/java/com/example/dockercicd/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/")
     public String getHome() {
-        return "Is Change WOW!";
+        return "Hello!";
     }
 }
